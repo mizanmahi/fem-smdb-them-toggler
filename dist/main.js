cda05d916fe1d798d7a72817fb785ea7ce0544c7
mizanmahi <mizanmahi24@gmail.com>
2021-07-30 22:35:10 +0600

    done with the responsiveness and toggle function

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,19 @@
-console.log("connected!");
+const toggleButton = document.getElementById("toggle-checkbox");
+const currentTheme = localStorage.getItem("theme");
+
+if (currentTheme) {
+   if (currentTheme === "light") {
+      toggleButton.checked = true;
+   }
+   document.documentElement.setAttribute("data-theme", currentTheme);
+}
+
+toggleButton.addEventListener("change", function () {
+   if (this.checked) {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
+   } else {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+   }
+});
